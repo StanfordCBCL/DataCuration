2b9391c7018bc6a35c314117740f6bc8bbe1cc68
Martin Pfaller <pfaller@stanford.edu>
2019-11-20 14:40:07 -0800

    cleanup of bcs integrals

--- get_bc_integrals.py
+++ get_bc_integrals.py
@@ -18,56 +18,22 @@ def read_geo(fname):
     return reader, reader.GetOutput().GetPointData(), reader.GetOutput().GetCellData()
 
 
-def read_faces(fpath_solve):
-    # read geometry
-    fname = os.path.join(fpath_solve, 'mesh-complete', 'all_exterior.vtp')
-    reader = read_geo(fname).GetOutput()
-
-    # get cell connectivity (can this be done without loops??)
-    cells = []
-    for i in range(reader.GetNumberOfCells()):
-        c = reader.GetCell(i)
-        points = []
-        for j in range(c.GetNumberOfPoints()):
-            points.append(c.GetPointIds().GetId(j))
-        cells.append(points)
-    cells = np.array(cells)
-
-    # extract face and global element/node IDs
-    faces = numpy_support.vtk_to_numpy(reader.GetCellData().GetArray('BC_FaceID'))
-    glob_elem_id = numpy_support.vtk_to_numpy(reader.GetCellData().GetArray('GlobalElementID'))
-    glob_node_id = numpy_support.vtk_to_numpy(reader.GetPointData().GetArray('GlobalNodeID')).astype(int)
-
-    # store
-    face_dic = {'cells': {}, 'points': {}}
-    for f in np.unique(faces):
-        # select all elements of face
-        id_elem = faces == f
-
-        # select all nodes of face
-        id_node = np.unique(cells[id_elem])
-
-        # store for later use
-        face_dic['cells'][f] = glob_elem_id[id_elem]
-        face_dic['points'][f] = glob_node_id[id_node]
-
-    return face_dic
+def write_geo(fname, reader):
+    writer = vtk.vtkXMLPolyDataWriter()
+    writer.SetFileName(fname)
+    writer.SetInputConnection(reader.GetOutputPort())
+    writer.Update()
+    writer.Write()
+    return
 
 
-def transfer_solution(fpath_surf, fpath_vol):
-    # read surface and volume mesh
-    reader_surf, node_surf, cell_surf = read_geo(fpath_surf)
-    reader_vol, node_vol, _ = read_geo(fpath_vol)
-
+def transfer_solution(node_surf, node_vol, res_fields):
     # get unique node ids in both meshes
     nd_id_surf = numpy_support.vtk_to_numpy(node_surf.GetArray('GlobalNodeID')).astype(int)
     nd_id_vol = numpy_support.vtk_to_numpy(node_vol.GetArray('GlobalNodeID')).astype(int)
 
     # map volume mesh to surface mesh
     mask = np.searchsorted(nd_id_vol, nd_id_surf)
-
-    # fields to extract
-    res_fields = ['velocity', 'pressure']
 
     # transfer results from volume mesh to surface mesh
     for i in range(node_vol.GetNumberOfArrays()):
@@ -76,11 +42,33 @@ def transfer_solution(fpath_surf, fpath_vol):
             # read results from volume mesh
             res = numpy_support.vtk_to_numpy(node_vol.GetArray(res_name))
 
-            # create array to output surface results
+            # create array to output surface mesh results
             out_array = numpy_support.numpy_to_vtk(res[mask])
             out_array.SetName(res_name)
             node_surf.AddArray(out_array)
 
+
+def sort_faces(res_faces):
+    # get time steps
+    times = []
+    for n in res_faces[1].keys():
+        times.append(float(n.split('_')[1]))
+    times = np.unique(np.array(times))
+
+    # sort data in arrays according to time steps
+    res_array = {'time': times}
+    for f, f_res in res_faces.items():
+        res_array[f] = {}
+        for res_name, res in f_res.items():
+            name, time = res_name.split('_')
+            if name not in res_array[f]:
+                res_array[f][name] = np.zeros(times.shape[0])
+            res_array[f][name][np.where(float(time) == times)] = res
+
+    return res_array
+
+
+def integrate_surfaces(reader_surf, cell_surf, res_fields):
     # boundary faces
     faces = np.unique(numpy_support.vtk_to_numpy(cell_surf.GetArray('BC_FaceID')).astype(int))
     res_faces = {}
@@ -114,48 +102,27 @@ def transfer_solution(fpath_surf, fpath_vol):
                     # export scalar
                     if len(out.shape) == 1 and out.shape[0] == 1:
                         res_faces[f][res_name] = out[0]
+
                     # export vector norm
                     elif len(out.shape) == 2 and out.shape[1] == 3:
                         res_faces[f][res_name] = np.linalg.norm(out)
                     else:
                         raise ValueError('Unknown shape of array ' + repr(out.shape))
 
-    # todo: sort according to time
-    keys = list(res_faces[1].keys())
-    names = []
-    times = []
-    for n in keys:
-        name, time = n.split('_')
-        names.append(name)
-        times.append(float(time))
-    names = np.unique(np.array(names))
-    times = np.unique(np.array(times))
-
-    res_faces_array = {'time': times}
-    for face, face_res in res_faces.items():
-        res_faces_array[face] = {}
-        for res_name, res in face_res.items():
-            name, time = res_name.split('_')
-            if name not in res_faces_array[face]:
-                res_faces_array[face][name] = np.zeros(times.shape[0])
-            res_faces_array[face][name][np.where(float(time) == times)] = res
-
-    pdb.set_trace()
-
-    # write results
-    writer = vtk.vtkXMLPolyDataWriter()
-    writer.SetFileName('test.vtp')
-    writer.SetInputConnection(reader_surf.GetOutputPort())
-    writer.Update()
-    writer.Write()
-
-    pdb.set_trace()
-
-    return
+    return sort_faces(res_faces)
 
 
-def int_surface(fpath_solve, fpath_res):
-    fpath_surf = os.path.join(fpath_solve, 'mesh-complete', 'all_exterior.vtp')
-    transfer_solution(fpath_surf, fpath_res)
+def integrate_bcs(fpath_surf, fpath_vol, res_fields):
+    # read surface and volume meshes
+    reader_surf, node_surf, cell_surf = read_geo(fpath_surf)
+    reader_vol, node_vol, _ = read_geo(fpath_vol)
 
-    return
+    # transfer solution from volume mesh to surface mesh
+    transfer_solution(node_surf, node_vol, res_fields)
+
+    # integrate data on boundary surfaces
+    res_faces = integrate_surfaces(reader_surf, cell_surf, res_fields)
+
+    # write results for debugging in paraview
+    # write_geo('test.vtp', reader_surf)
+    return res_faces
