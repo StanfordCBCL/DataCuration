ce996c51edb30d5e4c877df07af70c49b762f4fb
Martin Pfaller <pfaller@stanford.edu>
2020-01-08 10:45:47 -0800

    add file to handle all vtk functions

---
+++ vtk_functions.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+
+import os
+import vtk
+import pdb
+
+import numpy as np
+
+from vtk.util.numpy_support import numpy_to_vtk as n2v
+from vtk.util.numpy_support import vtk_to_numpy as v2n
+
+
+class Integration:
+    """
+    Class to perform integration on slices
+    """
+
+    def __init__(self, inp):
+        try:
+            self.integrator = vtk.vtkIntegrateAttributes()
+        except AttributeError:
+            raise Exception('vtkIntegrateAttributes is currently only supported by pvpython')
+
+        self.integrator.SetInputData(inp.GetOutput())
+        self.integrator.Update()
+
+    def evaluate(self, res_name):
+        """
+        Evaluate integral.
+        Distinguishes between scalar integration (e.g. pressure) and normal projection (velocity)
+        Optionally divides integral by integrated area
+        Args:
+            field: pressure, velocity, ...
+            res_name: name of array
+
+        Returns:
+            Scalar integral
+        """
+        # type of result
+        field = res_name.split('_')[0]
+
+        if field == 'velocity':
+            int_name = 'normal_' + res_name
+        else:
+            int_name = res_name
+
+        # evaluate integral
+        integral = v2n(self.integrator.GetOutput().GetPointData().GetArray(int_name))[0]
+
+        # choose if integral should be divided by area
+        if field == 'velocity':
+            return integral
+        else:
+            return integral / self.area()
+
+    def area(self):
+        """
+        Evaluate integrated surface area
+        Returns:
+        Area
+        """
+        return v2n(self.integrator.GetOutput().GetCellData().GetArray('Area'))[0]
+
+
+def read_geo(fname):
+    """
+    Read geometry from file, chose corresponding vtk reader
+    Args:
+        fname: vtp surface or vtu volume mesh
+
+    Returns:
+        vtk reader, point data, cell data
+    """
+    _, ext = os.path.splitext(fname)
+    if ext == '.vtp':
+        reader = vtk.vtkXMLPolyDataReader()
+    elif ext == '.vtu':
+        reader = vtk.vtkXMLUnstructuredGridReader()
+    else:
+        raise ValueError('File extension ' + ext + ' unknown.')
+    reader.SetFileName(fname)
+    reader.Update()
+    return reader, reader.GetOutput().GetPointData(), reader.GetOutput().GetCellData()
+
+
+def write_geo(fname, reader):
+    """
+    Write geometry to file
+    Args:
+        fname: file name
+        reader: vtkXMLPolyData
+    """
+    writer = vtk.vtkXMLPolyDataWriter()
+    writer.SetFileName(fname)
+    writer.SetInputConnection(reader.GetOutputPort())
+    writer.Update()
+    writer.Write()
+
+
+def threshold(inp, t, name):
+    """
+    Threshold according to cell array
+    Args:
+        inp: InputConnection
+        t: BC_FaceID
+        name: name in cell data used for thresholding
+    Returns:
+        reader, point data
+    """
+    thresh = vtk.vtkThreshold()
+    thresh.SetInputConnection(inp.GetOutputPort())
+    thresh.SetInputArrayToProcess(0, 0, 0, 1, name)
+    thresh.ThresholdBetween(t, t)
+    thresh.Update()
+    return thresh
+
+
+def calculator(inp, function, inp_arrays, out_array):
+    """
+    Function to add vtk calculator
+    Args:
+        inp: InputConnection
+        function: string with function expression
+        inp_arrays: list of input point data arrays
+        out_array: name of output array
+    Returns:
+        calc: calculator object
+    """
+    calc = vtk.vtkArrayCalculator()
+    for a in inp_arrays:
+        calc.AddVectorArrayName(a)
+    calc.SetInputData(inp.GetOutput())
+    calc.SetAttributeModeToUsePointData()
+    calc.SetFunction(function)
+    calc.SetResultArrayName(out_array)
+    calc.Update()
+    return calc
+
+
+def cut_plane(inp, origin, normal):
+    """
+    Cuts geometry at a plane
+    Args:
+        inp: InputConnection
+        origin: cutting plane origin
+        normal: cutting plane normal
+    Returns:
+        cut: cutter object
+    """
+    # define cutting plane
+    plane = vtk.vtkPlane()
+    plane.SetOrigin(origin[0], origin[1], origin[2])
+    plane.SetNormal(normal[0], normal[1], normal[2])
+
+    # define cutter
+    cut = vtk.vtkCutter()
+    cut.SetInputData(inp.GetOutput())
+    cut.SetCutFunction(plane)
+    cut.Update()
+    return cut
+
+
+def connectivity(inp, origin):
+    """
+    If there are more than one unconnected geometries, extract the closest one
+    Args:
+        inp: InputConnection
+        origin: region closest to this point will be extracted
+    Returns:
+        con: connectivity object
+    """
+    con = vtk.vtkConnectivityFilter()
+    con.SetInputData(inp.GetOutput())
+    con.SetExtractionModeToClosestPointRegion()
+    con.SetClosestPoint(origin[0], origin[1], origin[2])
+    con.Update()
+    return con
