12b16183dfe7499020f4a6e1edf3ea16839cc2bc
mrp089 <pfaller@stanford.edu>
2020-02-19 17:32:37 -0800

    write outlet names to file

--- get_centerline.py
+++ get_centerline.py
@@ -3,11 +3,15 @@
 import os
 import sys
 import pdb
+import shutil
 import subprocess
-from collections import OrderedDict
+import tempfile
 
-from get_database import Database, SimVascular
-from common import input_args
+from collections import OrderedDict
+import numpy as np
+
+from get_database import Database, SimVascular, input_args
+from vtk_functions import ClosestPoints
 
 sys.path.append('/home/pfaller/work/repos/SimVascular/Python/site-packages/')
 
@@ -33,34 +37,52 @@ def main(db, geometries):
     for geo in geometries:
         print('Running geometry ' + geo)
 
-        if not os.path.exists(db.get_centerline_path(geo)) or not os.path.exists(db.get_surfaces(geo, 'all_exterior')):
+        if not db.get_surfaces(geo, 'all_exterior'):
             continue
 
-        if os.path.exists(db.get_section_path(geo)):
-            continue
+        # if os.path.exists(db.get_section_path(geo)):
+        #     continue
 
         # get model paths
         p = OrderedDict()
-        p['cent'] = db.get_centerline_path(geo)
         p['surf'] = db.get_surfaces(geo, 'all_exterior')
-        p['lines'] = db.get_centerline_section_path(geo)
+        p['lines'] = db.get_centerline_path(geo)
         p['sections'] = db.get_section_path(geo)
+        p['surf_grouped'] = db.get_surfaces_grouped_path(geo)
+
+        # copy cap surfaces to temp folder
+        fpath_surf = tempfile.mkdtemp()
+        for f in db.get_surfaces(geo, 'caps'):
+            shutil.copy2(f, fpath_surf)
 
         # get inlet and outlet centers
-        params = Params(db.get_surface_dir(geo), os.path.basename(db.get_surfaces(geo, 'inflow')))
+        params = Params(fpath_surf, os.path.basename(db.get_surfaces(geo, 'inflow')))
         cl.get_inlet_outlet_centers(params)
-        p['inlet'] = repr(cl.inlet_center).replace(' ', '')
-        p['outlets'] = repr(cl.outlet_centers).replace(' ', '')
+
+        # remove temp dir
+        shutil.rmtree(fpath_surf)
+
+        # find corresponding point id
+        cp = ClosestPoints(p['surf'])
+        caps = np.vstack((cl.inlet_center, np.array(cl.outlet_centers).reshape(-1, 3)))
+        id_caps = cp.search(caps)
+
+        # convert for export
+        p['caps'] = repr(id_caps).replace(' ', '')
 
         # assemble call string
         sv_string = [os.path.join(os.getcwd(), 'get_centerline_sv.py')]
         for v in p.values():
             sv_string += [v]
 
-        print(sv_string)
+        # write outlet names to file
+        with open(db.get_centerline_outlet_path(geo), 'w+') as f:
+            for s in cl.outlet_face_names:
+                f.write(s + '\n')
 
         # execute SimVascular-Python
         sv.run_python(sv_string)
+        # sv.run_python_debug(sv_string)
 
 
 if __name__ == '__main__':
--- get_centerline_sv.py
+++ get_centerline_sv.py
@@ -5,44 +5,85 @@ try:
 except ImportError:
     raise ImportError('Run with sv --python -- this_script.py')
 
+import os
 import json
 import argparse
-import pdb
 
-def main(p):
-    # set SimVascular internal names (arbitrary)
-    names = {'cent': 'centerlines_in', 'surf': 'surface', 'lines': 'centerline_out', 'sections': 'sections_out'}
 
-    # get model paths
-    paths = {'cent': p.cent, 'surf': p.surf, 'lines': p.lines, 'sections': p.sections}
+def centerlines(p, params):
+    sv.Repository.ReadXMLPolyData('surf', p['surf'])
+    sv.VMTKUtils.Centerlines('surf', [params.caps[0]], params.caps[1:], 'tmp1', 'voronoi')
+    sv.Repository.WriteXMLPolyData('tmp1', p['lines'])
+    sv.Repository.Delete('surf')
+    sv.Repository.Delete('tmp1')
+    sv.Repository.Delete('voronoi')
 
-    # add files to repository
-    for k in ['cent', 'surf']:
-        sv.Repository.ReadXMLPolyData(names[k], paths[k])
 
-    # todo: execute complete VMTK centerline toolchain
+def separate(p):
+    sv.Repository.ReadXMLPolyData('tmp1', p['lines'])
+    sv.VMTKUtils.Separatecenterlines('tmp1', 'tmp2')
+    sv.Repository.WriteXMLPolyData('tmp2', p['lines'])
+    sv.Repository.Delete('tmp1')
+    sv.Repository.Delete('tmp2')
 
-    # execute VMTK function
-    sv.VMTKUtils.CenterlineSections(names['cent'], names['surf'], names['lines'], names['sections'])
 
-    # write from repository to file
-    for k in ['lines', 'sections']:
-        sv.Repository.WriteXMLPolyData(names[k], paths[k])
+def group(p):
+    sv.Repository.ReadXMLPolyData('surf', p['surf'])
+    sv.Repository.ReadXMLPolyData('tmp2', p['lines'])
+    sv.VMTKUtils.Grouppolydata('surf', 'tmp2', 'surf_grouped')
+    sv.Repository.WriteXMLPolyData('surf_grouped', p['surf_grouped'])
+    sv.Repository.Delete('surf')
+    sv.Repository.Delete('tmp2')
+    sv.Repository.Delete('surf_grouped')
 
-    # remove from repository
-    for v in names.values():
-        sv.Repository.Delete(v)
+
+def sections(p):
+    sv.Repository.ReadXMLPolyData('surf', p['surf'])
+    sv.Repository.ReadXMLPolyData('tmp2', p['lines'])
+    sv.Repository.ReadXMLPolyData('surf_grouped', p['surf_grouped'])
+    sv.VMTKUtils.CenterlineSections('tmp2', 'surf', 'surf_grouped', 'lines', 'sections')
+    sv.Repository.WriteXMLPolyData('lines', p['lines'])
+    sv.Repository.WriteXMLPolyData('sections', p['sections'])
+    sv.Repository.Delete('tmp2')
+    sv.Repository.Delete('surf_grouped')
+    sv.Repository.Delete('lines')
+    sv.Repository.Delete('sections')
+
+
+def main(params):
+    # get model parameters
+    p = {'surf': params.surf, 'lines': params.lines, 'surf_grouped': params.surf_grouped, 'sections': params.sections}
+
+    # execute VMTK centerline functions
+    if not os.path.exists(p['surf']):
+        print('Skipping (no surface mesh)')
+        return
+
+    if not os.path.exists(p['lines']):
+        centerlines(p, params)
+        separate(p)
+    else:
+        print('Found centerline')
+
+    if not os.path.exists(p['surf_grouped']):
+        group(p)
+    else:
+        print('Found surface grouped')
+
+    if not os.path.exists(p['sections']):
+        sections(p)
+    else:
+        print('Found sections')
 
 
 if __name__ == '__main__':
     # parse input arguments
     parser = argparse.ArgumentParser(description='generate centerline in SimVascular')
-    parser.add_argument('cent', help='input centerline')
     parser.add_argument('surf', help='input surface')
     parser.add_argument('lines', help='output centerline')
     parser.add_argument('sections', help='output sections')
-    parser.add_argument('inlet', type=json.loads, help='inlet center')
-    parser.add_argument('outlets', type=json.loads, help='outlet centers')
+    parser.add_argument('surf_grouped', help='output surface grouped')
+    parser.add_argument('caps', type=json.loads, help='cap point ids')
 
     # run script
     main(parser.parse_args())
