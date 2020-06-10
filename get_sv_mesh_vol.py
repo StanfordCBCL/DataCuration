bdfdf4d9517a2631ed1d6d1bfa1238fb61e16788
mrp089 <pfaller@stanford.edu>
2020-06-09 18:16:13 -0700

    check jacobian before flipping nodes

--- get_sv_mesh_vol.py
+++ get_sv_mesh_vol.py
@@ -11,6 +11,16 @@ from vtk_functions import read_geo, cell_connectivity
 from get_database import input_args
 
 
+def jacobian_positive(points, tets):
+    """
+    Check if Jacobian determinant is positive everywhere
+    """
+    jac = np.zeros((tets.shape[0], 3, 3))
+    for i, p in enumerate([[1, 0], [2, 0], [3, 0]]):
+        jac[:, :, i] = points[tets[:, p[0]]] - points[tets[:, p[1]]]
+    return np.sum(np.linalg.det(jac) <= 0.0) == 0
+
+
 def main(db, geometries):
     """
     Generate volume mesh for SimVascular: remove all unused arrays and reoder tet nodes
@@ -19,6 +29,7 @@ def main(db, geometries):
         print('Running geometry ' + geo)
 
         if not os.path.exists(db.get_volume(geo)):
+            print('  no volume mesh')
             continue
 
         # read volume mesh
@@ -29,15 +40,15 @@ def main(db, geometries):
         cells = cell_connectivity(vol)
 
         # reorder nodes in tets to fix negative Jacobian
-        tets = cells['tetra'][:, [0, 1, 3, 2]]
-        cells['tetra'] = tets
+        if not jacobian_positive(points, cells['tetra']):
+            cells['tetra'] = cells['tetra'][:, [0, 1, 3, 2]]
+            print('  tets flipped')
+        else:
+            print('  tets ok')
+        # assert jacobian_positive(points, cells['tetra']), 'Jacobian negative after flipping tets'
 
-        # check if Jacobian determinant is positive everywhere
-        jac = np.zeros((tets.shape[0], 3, 3))
-        for i, p in enumerate([[1, 0], [2, 0], [3, 0]]):
-            jac[:, :, i] = points[tets[:, p[0]]] - points[tets[:, p[1]]]
-        if np.sum(np.linalg.det(jac) <= 0) != 0:
-            print('determinant is negative')
+        if not jacobian_positive(points, cells['tetra']):
+            print('  Jacobian negative after flipping tets')
 
         # get arrays
         point_data = {'GlobalNodeID': np.expand_dims(v2n(vol.GetPointData().GetArray('GlobalNodeID')), axis=1)}
