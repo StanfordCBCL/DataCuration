28e08029b154b915670c54ea322a036cf3cd9745
mrp089 <pfaller@stanford.edu>
2020-06-12 08:29:55 -0700

    cleanup

--- vtk_to_xdmf.py
+++ vtk_to_xdmf.py
@@ -5,10 +5,11 @@ import meshio
 import numpy as np
 import pdb
 
+from collections import defaultdict
+from vtk.util.numpy_support import vtk_to_numpy as v2n
+
 from vtk_functions import read_geo, get_all_arrays, cell_connectivity
 from get_database import Database, input_args
-
-from vtk.util.numpy_support import vtk_to_numpy as v2n
 
 
 def split(array):
@@ -40,31 +41,61 @@ def convert_data(data):
         return data
 
 
-def convert(f_in, f_out):
+def collect_arrays(array_type, input_arrays, arrays):
+    for array_name, data in input_arrays.items():
+        time, name = split(array_name)
+        arrays[time][array_type][name] = convert_data(data)
+
+
+def get_time_series(geo):
+    # read geometry
+    point_arrays, cell_arrays = get_all_arrays(geo)
+
+    # collect all arrays
+    rec_dd = lambda: defaultdict(rec_dd)
+    arrays = rec_dd()
+    collect_arrays('point', point_arrays, arrays)
+    collect_arrays('cell', cell_arrays, arrays)
+    return arrays
+
+
+def write_xdmf(geo, arrays, f_out):
     """
     Convert .vtu/.vtp to xdmf
     """
-    # read geometry
-    geo = read_geo(f_in).GetOutput()
-    point_arrays, cell_arrays = get_all_arrays(geo)
-
     # extract connectivity
     cells = cell_connectivity(geo)
     points = v2n(geo.GetPoints().GetData())
+
+    # collect time steps
+    times = np.unique(list(arrays.keys())).tolist()
+    if 0 in times:
+        times.remove(0)
+    if len(times) == 0:
+        times = [0.0]
 
     with meshio.xdmf.TimeSeriesWriter(f_out) as writer:
         # write points and cells
         writer.write_points_cells(points, cells)
 
-        # write point data
-        for array, data in point_arrays.items():
-            time, name = split(array)
-            writer.write_data(time, point_data={name: convert_data(data)})
+        # write arrays
+        for time, data in arrays.items():
+            if time == '0':
+                for t in times:
+                    writer.write_data(t, point_data=data['point'], cell_data=data['cell'])
+            else:
+                writer.write_data(time, point_data=data['point'], cell_data=data['cell'])
 
-        # write cell data
-        for array, data in cell_arrays.items():
-            time, name = split(array)
-            writer.write_data(time, cell_data={name: convert_data(data)})
+
+def osmsc_to_xdmf(f_in, f_out):
+    # read geometry
+    geo = read_geo(f_in).GetOutput()
+
+    # extract time data
+    arrays = get_time_series(geo)
+
+    # write to file
+    write_xdmf(geo, arrays, f_out)
 
 
 def main(db, geometries):
@@ -80,7 +111,7 @@ def main(db, geometries):
         if not os.path.exists(f_in):
             continue
 
-        convert(f_in, f_out)
+        osmsc_to_xdmf(f_in, f_out)
 
 
 if __name__ == '__main__':
