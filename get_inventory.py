1e491af316997cb5f29ccdb90374cdabfb46711f
mrp089 <pfaller@stanford.edu>
2020-11-30 16:21:01 -0800

    calculate time constants

--- get_inventory.py
+++ get_inventory.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 
 import pdb
+import os
 import csv
+import numpy as np
 from collections import defaultdict, OrderedDict
 
 from get_database import Database
+from get_sv_project import coronary_sv_to_oned
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
 
 
 def main():
@@ -20,13 +26,13 @@ def main():
     # extract simulation parameters
     for geo in geometries:
         print('Running geometry ' + geo)
-        params = db.get_params(geo)
+        bc_def = db.get_bcs(geo)
 
         for pm in params_read:
-            if params is None or pm not in params:
+            if bc_def['params'] is None or pm not in bc_def['params']:
                 val = 'unknown'
             else:
-                val = params[pm]
+                val = bc_def['params'][pm]
             properties[geo][pm] = val
 
     # check if simulation is to be published
@@ -57,5 +63,80 @@ def main():
             reader.writerow([geo] + [v for v in properties[geo].values()])
 
 
+def time_constant():
+    db = Database()
+    # geometries = db.get_geometries()
+    geometries = db.get_geometries_select('paper')
+
+    # color by category
+    colors = {'Cerebrovascular': 'k',
+              'Coronary': 'r',
+              'Aortofemoral': 'm',
+              'Pulmonary': 'c',
+              'Congenital Heart Disease': 'y',
+              'Aorta': 'b',
+              'Animal and Misc': '0.75'}
+
+    fig1, ax1 = plt.subplots(dpi=400, figsize=(15, 6))
+
+    geos = []
+    i = 0
+    for geo in geometries:
+        params = db.get_bcs(geo)
+        time, _ = db.get_inflow(geo)
+
+        # skip geometries without BCs
+        if params is None:
+            continue
+
+        col = colors[params['params']['deliverable_category']]
+
+        # collect all time constants
+        tau_bc = []
+        for cp, bc in params['bc'].items():
+            if 'Rd' in bc:
+                tau = bc['Rd'] * bc['C']
+                m = '_'
+            elif 'Pim' in bc:
+                p = {}
+                cor = coronary_sv_to_oned(bc)
+                p['R1'], p['R2'], p['R3'], p['C1'], p['C2'] = (cor['Ra1'], cor['Ra2'], cor['Rv1'], cor['Ca'], cor['Cc'])
+                tau1 = p['C1'] / (1 / (p['R2'] + p['R1']) + 1 / p['R3'])
+                tau2 = p['C2'] / (1 / (p['R2'] + p['R3']) + 1 / p['R1'])
+                tau = tau1 + tau2
+                if 'l' in cp.lower():
+                    m = 'x'
+                else:
+                    m = 'o'
+            else:
+                continue
+
+            #  tau in cardiac cycles
+            tau /= time[-1]
+
+            ax1.plot(i, tau, marker=m, color=col)
+            tau_bc += [tau]
+
+        # skip geometries without RCR
+        if len(tau_bc) == 0:
+            continue
+
+        # ax1.boxplot(tau_bc, positions=[i])
+        ax1.plot([i, i], [np.min(tau_bc), np.max(tau_bc)], '-', color=col)
+
+        geos += [geo]
+        i += 1
+
+    # plt.yscale('log')
+    ax1.xaxis.grid('minor')
+    ax1.yaxis.grid(True)
+    ax1.set_ylim(0, 13)
+    plt.xticks(np.arange(len(geos)), geos, rotation='vertical')
+    plt.ylabel('Time constant [cycles]')
+    fname = os.path.join(db.fpath_gen, 'time_constants.png')
+    fig1.savefig(fname, bbox_inches='tight')
+
+
 if __name__ == '__main__':
-    main()
+    # main()
+    time_constant()
